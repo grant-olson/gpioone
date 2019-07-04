d43176986f97af1442f625c1f3d7e4ee836d9b63
Grant T. Olson <kgo@grant-olson.net>
2019-07-04 10:07:58 -0400

    7 Segemnt display output for shift register

--- shift_register.py
+++ shift_register.py
@@ -1,6 +1,66 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+class SegmentDisplay:
+    # Wire up shift register in this order!
+    A = 1 << 0 # Top vertical
+    B = 1 << 1 # Top Right Side
+    C = 1 << 2 # Bottom Right Side
+    D = 1 << 3 # Bottom Vertical
+    E = 1 << 4 # Bottom left side    
+    F = 1 << 5 # Top left side
+    G = 1 << 6 # Middle Vertical
+    DP = 1 << 7 # Decimal Point
+
+    DISPLAY = {
+        "0": A + B + C + D + E + F,
+        "1": B + C,
+        "2": A + B + G + E + D,
+        "3": A + B + C + D + G,
+        "4": F + G + B + C,
+        "5": A + F + G + C + D,
+        "6": F + E + G + D + C,
+        "7": A + B + C,
+        "8": A + B + C + D + E + F + G,
+        "9": A + B + G + F + C,
+
+        "A": A + B + F + G + E + C,
+        "B": A + B + C + D + E + F + G,
+        "C": A + F + E + D,
+        "D": B + C + D + G + E,
+        "E": A + F + G + E + D,
+        "F": A + F + G + E,
+        "G": A + B + F + G + C + D,
+        "H": F + B + G + E + C,
+        "I": F + E,
+        "J": B + C + D + E,
+        "L": F + E + D,
+        "N": E + G + C,
+        "O": E + G + C + D,
+        "P": A + B + G + F + E,
+        "R": E + G,
+        "S": A + F + G + C + D,
+        "T": B + G + C,
+        "U": E + D + C,
+        "V": E + D + C,
+        "Y": F + G + B + C,
+        
+        
+        "?": A + B + G + E
+        }
+
+    @staticmethod
+    def value(character, decimal_point=False):
+        if len(character) <> 1:
+            raise "Only one char!"
+        if character in SegmentDisplay.DISPLAY:
+            v = SegmentDisplay.DISPLAY[character.upper()]
+        else:
+            v = SegmentDisplay.DISPLAY["?"]
+        if decimal_point:
+            v += SegmentDisplay.DP
+        return v
+        
 class ShiftRegister:
     """
     Interface for a shift register such as 74HC595 or clone
@@ -47,15 +107,10 @@ class ShiftRegister:
             self.clock_tick()
         self.trigger_latch()
 
-if __name__ == "__main__":
-    GPIO.setmode(GPIO.BCM)
+    def character(self, c, decimal_point=False):
+        self.number(SegmentDisplay.value(c,decimal_point=decimal_point))
 
-    latch = 20 # 21
-    clock = 16 # 20
-    data = 21 # 16
-    
-    shift_register = ShiftRegister(data, clock, latch)
-
+def led_strip_example(shift_register):
     # Example is hooked up to 8 LEDs with 220 Ohm resistors between
     # anode and Shift Register pins.
 
@@ -78,5 +133,31 @@ if __name__ == "__main__":
             shift_register.clock_tick()
             shift_register.trigger_latch()
             sleep(sleep_time)
+
+def segment_example(shift_register):
+    for i in range(0,10):
+        if i % 2:
+            shift_register.number(SegmentDisplay.value(str(i)))
+        else:
+            shift_register.number(SegmentDisplay.value(str(i), decimal_point=True))
+        sleep(0.5)
+
+    counter = 0
+    for c in "ABCDEFGHIJKLMNOPQRSTUVWXZY":
+        counter += 1
+        shift_register.character(c, decimal_point=(counter / 2))
+        sleep(0.5)
+
+    shift_register.number(0)
+            
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
+
+    latch = 20 # 21
+    clock = 16 # 20
+    data = 21 # 16
+    
+    shift_register = ShiftRegister(data, clock, latch)
+    segment_example(shift_register)
     
     GPIO.cleanup()
