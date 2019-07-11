c1b4420d7f4f9fc1fc35c4276e2db052da7ad8c0
Grant T. Olson <grant@pikimal.com>
2019-07-10 21:59:12 -0400

    Initial PyPI release

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gpioone",
+    version="0.1.0",
+    author="Grant T. Olson",
+    author_email="kgo@grant-olson.net",
+    description="Interfaces for various GPIO devices on Raspberry PI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grant-olson/gpioone",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
