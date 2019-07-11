bdaa07ad9089aee859340d611a8f27d996abddf3
Grant T. Olson <kgo@grant-olson.net>
2019-07-10 21:33:19 -0400

    Fix shift register example to use env vars

--- examples/shift_register.py
+++ examples/shift_register.py
@@ -2,8 +2,6 @@
 # from gpioone import *
 from gpioone_setup import *
 
-raise SystemError("not ported to new example structure yet")
-
 def led_strip_example(shift_register):
     # Example is hooked up to 8 LEDs with 220 Ohm resistors between
     # anode and Shift Register pins.
@@ -45,19 +43,13 @@ def segment_example(shift_register):
     shift_register.number(0)
 
 
-def multi_segment_example(shift_register):
+def multi_segment_example(shift_register, first, second, third, fourth):
     """
     Use four IO pins to control a 4 digit seven segment display.
 
     TODO: Chain two shift registers together for the slot control
     logic to free the pins back up.
     """
-    first = 18
-    second = 19
-    third = 23
-    fourth = 24
-
-    #19, 18 ,23, 25
     GPIO.setup(first, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.setup(second, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.setup(third, GPIO.OUT, initial=GPIO.HIGH)
@@ -93,10 +85,57 @@ def multi_segment_example(shift_register):
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
 
-    latch = 20 # 21
-    clock = 16 # 20
-    data = 21 # 16
+# This is a harness to run from source, if using the installed package use:
+# from gpioone import *
+from gpioone_setup import * 
+
+if __name__ == "__main__":
+
+    s = SetupExample(help="""This provides a few examples of using a shift register.
+
+Don't forget to include resistors where appropriate on all LEDs!
+
+Set LEDS to 1 to have the shift register display on 8 LEDs tied to outputs.
+
+Set SEGMENT to 1 count up on a single seven-segment LED display.
+
+Set MULTI_1 - MULTI_4 to display a multisegment LED Display with the GPIO pins
+that toggle the active display.
+
+Note on multi-segment setup, shift register outputs 0-6 map to LED outputs
+A-G respectively. 7 maps to the DP LED. Consult a datasheet if you're not sure
+which pins map to what segment.
+""")
+
+    s.rv("DATA", "Data Pin on Shift Register")
+    s.rv("LATCH", "Latch Pin on Shift Register")
+    s.rv("CLOCK", "Clock Pin on Shift Register")
+
+    s.ov("LEDS", "Set to one to display binary count on 8 LEDs.")
+
+    s.ov("SEGMENT", "Set to one to display on single digit seven segment LED.")
     
-    shift_register = ShiftRegister(data, clock, latch)
-    multi_segment_example(shift_register)
-    GPIO.cleanup()
+    s.ov("MULTI_1", "Multisegment LED Display Digit 1")
+    s.ov("MULTI_2", "Multisegment LED Display Digit 2")
+    s.ov("MULTI_3", "Multisegment LED Display Digit 3")
+    s.ov("MULTI_4", "Multisegment LED Display Digit 4")
+         
+    
+    s.setup()
+
+    try:
+        shift_register = ShiftRegister(s.DATA, s.CLOCK, s.LATCH)
+
+        if hasattr(s,"MULTI_1"):
+            multi_segment_example(shift_register, s.MULTI_1, s.MULTI_2, s.MULTI_3, s.MULTI_4)
+        elif hasattr(s,"LEDS"):
+            led_strip_example(shift_register)
+        elif hasattr(s,"SEGMENT"):
+            segment_example(shift_register)
+        else:
+            print("You didn't tell us where to send the Segment Register output!")
+            
+    except KeyboardInterrupt:
+        pass
+
+GPIO.cleanup()
