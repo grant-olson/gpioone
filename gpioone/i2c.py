2cc0985c20c7724d6102d3e311ead59394d45ba3
Grant T. Olson <kgo@grant-olson.net>
2019-07-29 21:43:06 -0400

    Interface improvements to i2c

--- gpioone/i2c.py
+++ gpioone/i2c.py
@@ -1,4 +1,5 @@
 import smbus
+import numbers
 
 class I2C(object):
     def __init__(self, device_address):
@@ -9,7 +10,10 @@ class I2C(object):
         {}
 
     def register(self, register_name):
-        return self.registers()[register_name]
+        if isinstance(register_name, numbers.Number):
+            return register_name
+        else:
+            return self.registers()[register_name]
 
     def r(self, register_name):
         return self.register(register_name)
@@ -19,4 +23,10 @@ class I2C(object):
 
     def read_byte_data(self, register):
         return self.smb.read_byte_data(self.device_address, self.r(register))
+        
+    def write_block_data(self, register, *data):
+        self.smb.write_block_data(self.device_address, self.r(register), list(data))
+
+#    def read_block_data(self, register):
+#        return self.smb.read_block_data(self.device_address, self.r(register))
         
